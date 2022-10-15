4125d2e0e05a769d692ce95356da114b3bb36a88
GavinVasandani <89224315+GavinVasandani@users.noreply.github.com>
2022-10-15 11:07:21 -0800

    Added hello world file

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstring>
+#include <list>
+
+using namespace std;
+
+int main() {
+
+    cout<<"Hello World!"<<endl;
+
+
+}

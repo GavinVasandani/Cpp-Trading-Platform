bf76db27f0a0841da37585959f7baae8de362740
GavinVasandani <89224315+GavinVasandani@users.noreply.github.com>
2022-10-31 08:05:47 -0800

    Added network configs

--- ClientInput.cpp
+++ ClientInput.cpp
@@ -9,6 +9,15 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <messageObject.h>
+
+#ifdef _WIN32
+#define _WIN32_WINNT 0x0A00
+#endif
+
+#define ASIO_STANDALONE
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
 #include <serverObject.h>
 
 //C++ headers used in the scalping trading bot
